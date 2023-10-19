52fcee21badeb6c1554ba18458d8c0fa0ae245bd
alecon99 <alessioconte45@gmail.com>
2023-10-19 09:35:02 +0200

    V1.1

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -8,7 +8,9 @@ const NavigationBar = () => {
     return (
         <div className='bg-black fixed-top'>
             <Container className='d-flex py-3 align-items-center justify-content-between text-white'>
-                <img className='logo-small' src={logo} alt="logo Ac" />
+                <a href="#">
+                    <img className='logo-small' src={logo} alt="logo Ac" />
+                </a>
                 <div className='d-flex'>
                     <a href='#about' className='text-decoration-none text-white hover_orange'>About</a>
                     <a href='#portfolio' className='text-decoration-none text-white mx-5 hover_orange'>Portfolio</a>
