0a68cb50a77a840a9eca8c84dd0a4b1fcd11bf82
alecon99 <alessioconte45@gmail.com>
2023-10-25 14:19:15 +0200

    V1.4

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+
+import '../Footer/Footer.css'
+
+import { ProgressBar } from 'react-bootstrap';
 
 import logo from '../../media/ac.png'
 
 const Footer = () => {
+
+    const [ scroll, setScroll ] = useState(0)
+
+    window.onscroll = ()=>myFunction()
+
+    const myFunction = ()=> {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
+        setScroll(scrolled)
+    }
+
     return (
-        <div className='text-center fixed-bottom bg-black py-2 d-flex justify-content-center align-items-center'>
-            <img className='logo_xsmall' src={logo} alt="logo Ac" />
-            <div className='text-white ms-2'>Alessio Conte</div>
+        <div className='fixed-bottom bg-black'>
+            <ProgressBar variant="warning" now={scroll} id='progressBar' className='bg-black' />
+            <div className='text-center  py-2 d-flex justify-content-center align-items-center'>
+                <img className='logo_xsmall' src={logo} alt="logo Ac" />
+                <div className='text-white ms-2'>Alessio Conte</div>
+            </div>
         </div>
+
     )
 }
 
